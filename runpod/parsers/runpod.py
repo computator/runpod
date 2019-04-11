89200ae485ae91ea3a8ddb18274f4a6001c00738
Reuben Lifshay <rlifshay@gmail.com>
2019-04-11 01:05:27 -0700

    Runpod parser loads data from file.

--- runpod/parsers/runpod.py
+++ runpod/parsers/runpod.py
@@ -1,8 +1,17 @@
 from __future__ import absolute_import, print_function, unicode_literals
+import six
+import yaml
+
 from runpod.parsers import ParseError
 
 def parse_file(src_file):
-    return parse_object({})
+    try:
+        data = yaml.safe_load(src_file)
+    except yaml.YAMLError as e:
+        six.raise_from(ParseError("YAML parse error: " + str(e)), e)
+    if data is None:
+        raise ParseError("File must not be empty")
+    return parse_object(data)
 
-def parse_object(pod_object):
-    return pod_object
+def parse_object(data):
+    return data
