e2344e5dfea4dc075e8625a0ca78a89204d422ca
Reuben Lifshay <rlifshay@gmail.com>
2019-04-12 21:32:45 -0700

    Add add_container.

--- runpod/podmap/pod.py
+++ runpod/podmap/pod.py
@@ -7,10 +7,15 @@ class Pod(object):
         if containers:
             containers = list(containers)
             if any(True for ctr in containers if not isinstance(ctr, Container)):
-                raise TypeError("'containers' must be an iterable of <{}> instances".format(Container.__name__))
+                raise TypeError("'containers' must be an iterable of {} instances".format(Container.__name__))
             self.containers = containers
         else:
             self.containers = []
 
     def __repr__(self):
         return '<{} containers:{!r}>'.format(__class__.__name__, self.containers)
+
+    def add_container(self, container):
+        if not isinstance(container, Container):
+            raise TypeError("{!r} is not a {} instance".format(container, Container.__name__))
+        self.containers.append(container)
