81f5bdbbae3be7eeeac8fb5dc594a31542f5e5bd
Reuben Lifshay <rlifshay@gmail.com>
2019-06-29 20:36:05 -0700

    Add exposed ports to container.

--- runpod/podmap/container.py
+++ runpod/podmap/container.py
@@ -19,5 +19,30 @@ class Container(object):
             else:
                 raise RuntimeError("Image ID '{0.id}' for image {0} does not match expected format".format(image))
 
+        self._exposed_ports = set();
+
     def __repr__(self):
         return "<{} name:'{}' image:{}>".format(__class__.__name__, self.name, self.image)
+
+    @property
+    def exposed_ports(self):
+        return self._exposed_ports.copy()
+
+    def add_exposed_ports(self, ports):
+        ports = list(ports)
+        if any(True for port in ports if not isinstance(port, (tuple, int))):
+            raise TypeError("ports: iterable can only contain values of type tuple or int")
+        for portrange in [port for port in ports if isinstance(port, tuple)]:
+            if len(portrange) != 2:
+                raise TypeError("ports: tuple port ranges must be of length 2")
+            if any(True for port in portrange if not isinstance(port, int)):
+                raise TypeError("ports: tuple port ranges can only contain values of type int")
+        new_ports = set()
+        for port in ports:
+            if isinstance(port, tuple):
+                if port[0] <= port[1]:
+                    raise ValueError("ports: end value must be greater than start in tuple port ranges")
+                new_ports.update(range(port[0], port[1]+1))
+            else:
+                new_ports.add(port)
+        self._exposed_ports.update(new_ports)
