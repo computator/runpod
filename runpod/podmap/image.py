4d8159b67b3e2ea2e0bbb6ccaf84a1c63f039306
Reuben Lifshay <rlifshay@gmail.com>
2019-09-08 00:15:26 -0700

    Convert image to use attrs.

--- runpod/podmap/image.py
+++ runpod/podmap/image.py
@@ -1,30 +1,20 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import abc
+import attr
 import os.path
 import six
 from six.moves.urllib import parse as urllib_parse
 
 @six.add_metaclass(abc.ABCMeta)
+@attr.s
 class ImageBase(object):
-    @abc.abstractmethod
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        return '<{}>'.format(type(self).__name__)
-
     @abc.abstractproperty
     def id(self):
         pass
 
+@attr.s
 class DockerfileImage(ImageBase):
-    def __init__(self, dockerfile):
-        if not isinstance(dockerfile, six.text_type):
-            raise TypeError("expected {}, got {}".format(six.text_type.__name__, type(dockerfile).__name__))
-        self.source = dockerfile
-
-    def __repr__(self):
-        return "<{} source:'{}'>".format(type(self).__name__, self.source)
+    source = attr.ib(validator=attr.validators.instance_of(six.text_type))
 
     @property
     def id(self):
@@ -37,17 +27,10 @@ class DockerfileImage(ImageBase):
             path = os.path.abspath(self.source)
         return six.text_type(os.path.basename(os.path.dirname(path)))
 
+@attr.s
 class NamedImage(ImageBase):
-    def __init__(self, image, tag='latest'):
-        if not isinstance(image, six.text_type):
-            raise TypeError("image: expected {} but got {}".format(six.text_type.__name__, type(image).__name__))
-        if not isinstance(tag, six.text_type):
-            raise TypeError("tag: expected {} but got {}".format(six.text_type.__name__, type(tag).__name__))
-        self.image = image
-        self.tag = tag
-
-    def __repr__(self):
-        return "<{} ref:'{}:{}'>".format(type(self).__name__, self.image, self.tag)
+    image = attr.ib(validator=attr.validators.instance_of(six.text_type))
+    tag = attr.ib(default='latest', validator=attr.validators.instance_of(six.text_type))
 
     @property
     def id(self):
