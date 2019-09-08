00ca733f1239ac4efa651416d21f3685a311758e
Reuben Lifshay <rlifshay@gmail.com>
2019-09-07 23:00:30 -0700

    Convert pod to use attrs.

--- runpod/podmap/pod.py
+++ runpod/podmap/pod.py
@@ -1,19 +1,11 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+import attr
 from runpod.podmap.container import Container
 
+@attr.s
 class Pod(object):
-    def __init__(self, containers=None):
-        if containers:
-            containers = list(containers)
-            if any(True for ctr in containers if not isinstance(ctr, Container)):
-                raise TypeError("'containers' must be an iterable of {} instances".format(Container.__name__))
-            self.containers = containers
-        else:
-            self.containers = []
-
-    def __repr__(self):
-        return '<{} containers:{!r}>'.format(__class__.__name__, self.containers)
+    containers = attr.ib(factory=list, validator=attr.validators.deep_iterable(attr.validators.instance_of(Container)))
 
     def add_container(self, container):
         if not isinstance(container, Container):
