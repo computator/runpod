fc3b7647182de70398c3e28b866d28c263a75e37
Reuben Lifshay <rlifshay@gmail.com>
2019-09-07 23:57:23 -0700

    Convert container to use attrs.

--- runpod/podmap/container.py
+++ runpod/podmap/container.py
@@ -1,54 +1,52 @@
 from __future__ import absolute_import, print_function, unicode_literals
+import attr
 import re
+import six
 
 from runpod.podmap.image import ImageBase
 
+@attr.s(repr=False)
 class Container(object):
-    def __init__(self, image, name=None):
-        if not isinstance(image, ImageBase):
-            raise TypeError("image: expected {} but got {}".format(ImageBase.__name__, type(image).__name__))
-        self.image = image
-        if name is not None:
-            if not isinstance(name, six.text_type):
-                raise TypeError("name: expected {} but got {}".format(six.text_type.__name__, type(name).__name__))
-            self.name = name
-        else:
-            match = re.match('(?:.*/)?([^/:]*)(?::.*)?', image.id)
-            if match:
-                self.name = match.group(1)
-            else:
-                raise RuntimeError("Image ID '{0.id}' for image {0} does not match expected format".format(image))
+    image = attr.ib(validator=attr.validators.instance_of(ImageBase))
+    name = attr.ib(validator=attr.validators.instance_of(six.text_type))
 
-        self.annotations = {}
-        self.extra_hosts = {}
-        self.capabilities_add = set()
-        self.capabilities_drop = set()
-        self.devices = []
-        self.dns_enabled = True
-        self.dns_options = {}
-        self.dns_search = []
-        self.dns_servers = []
-        self.entrypoint = None
-        self.env = {}
-        self.env_file = None
-        self.exposed_ports = set()
-        self.hostname = None
-        self.ip = None
-        self.labels = {}
-        self.label_file = None
-        self.mounts = []
-        self.network = None
-        self.no_hosts = False
-        self.privileged = False
-        self.publish = []
-        self.publish_all = False
-        self.read_only = False
-        self.security_opts = []
-        self.tmpfs_mounts = []
-        self.user = None
-        self.volumes = []
-        self.volumes_from = None
-        self.working_directory = None
+    @name.default
+    def _name_def(self):
+        match = re.match('(?:.*/)?([^/:]*)(?::.*)?', self.image.id)
+        if match:
+            return match.group(1)
+        raise RuntimeError("Image ID '{0.id}' for image {0} does not match expected format".format(self.image))
+
+    annotations = attr.ib(factory=dict, init=False)
+    extra_hosts = attr.ib(factory=dict, init=False)
+    capabilities_add = attr.ib(default=set, init=False)
+    capabilities_drop = attr.ib(default=set, init=False)
+    devices = attr.ib(factory=list, init=False)
+    dns_enabled = attr.ib(default=True, init=False)
+    dns_options = attr.ib(factory=dict, init=False)
+    dns_search = attr.ib(factory=list, init=False)
+    dns_servers = attr.ib(factory=list, init=False)
+    entrypoint = attr.ib(default=None, init=False)
+    env = attr.ib(factory=dict, init=False)
+    env_file = attr.ib(default=None, init=False)
+    exposed_ports = attr.ib(default=set, init=False)
+    hostname = attr.ib(default=None, init=False)
+    ip = attr.ib(default=None, init=False)
+    labels = attr.ib(factory=dict, init=False)
+    label_file = attr.ib(default=None, init=False)
+    mounts = attr.ib(factory=list, init=False)
+    network = attr.ib(default=None, init=False)
+    no_hosts = attr.ib(default=False, init=False)
+    privileged = attr.ib(default=False, init=False)
+    publish = attr.ib(factory=list, init=False)
+    publish_all = attr.ib(default=False, init=False)
+    read_only = attr.ib(default=False, init=False)
+    security_opts = attr.ib(factory=list, init=False)
+    tmpfs_mounts = attr.ib(factory=list, init=False)
+    user = attr.ib(default=None, init=False)
+    volumes = attr.ib(factory=list, init=False)
+    volumes_from = attr.ib(default=None, init=False)
+    working_directory = attr.ib(default=None, init=False)
 
     def __repr__(self):
-        return "<{} name:'{}' image:{}>".format(type(self).__name__, self.name, self.image)
+        return "{}(name={!r}, image={!r}>".format(type(self).__name__, self.name, self.image)
