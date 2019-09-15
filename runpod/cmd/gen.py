88277e65d65c4d272eb598cfac65c72d86fbff4e
Reuben Lifshay <rlifshay@gmail.com>
2019-09-14 23:13:04 -0700

    Set helptext for the gen command.

--- runpod/cmd/gen.py
+++ runpod/cmd/gen.py
@@ -1,11 +1,15 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+CMD_HELP = {
+        'gen': "generate service config files for a pod"
+    }
+
 def get_cmd_names():
     return ('gen',)
 
 def init_cmd_subparsers(add_parser):
-    parser = add_parser("gen")
-    parser.add_argument('gen_format', choices=("systemd",))
+    parser = add_parser("gen", help=CMD_HELP['gen'])
+    parser.add_argument('gen_format', choices=("systemd",), help="set the output format for the service file(s)")
 
 def cmd_gen(podspec, opts):
     print(podspec, opts)
